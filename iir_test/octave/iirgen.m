4d294ae0c980dc4cc5d72ecefb2a36aa8ab4f421
pete <peter.dudley@precisionplanting.com>
2021-07-18 15:22:10 -0400

    octave SOS implementation matches ideal

--- iir_test/octave/iirgen.m
+++ iir_test/octave/iirgen.m
@@ -1,5 +1,5 @@
 clear; 
-N=8; 
+N=8; % must be multiple of two for second order sections
 Wc=0.25; %0.02; 
 
 coeff_int = 2;
@@ -30,9 +30,41 @@ if (max_q_coeff_int > (2^(coeff_int-2))) printf("ERROR: not enough integer bits.
 % look at the quantized filter
 [b_q, a_q] = sos2tf(sos_q_scaled);
 %zplane(b_q, a_q);
-%freqz(b_q, a_q);
+%freqz(b_q, a_q, 4096);
 
 % simulate the filter.
+Nsim = 64;
+s=zeros(1,Nsim); s(10) = 1; % impulse
+
+% direct implementation
+s_filt = filter(b_q,a_q,s);
+
+% cascaded SOS
+s_filt2 = s;
+for i=1:N/2
+    b_sos = sos_q_scaled(i,1:3);
+    a_sos = sos_q_scaled(i,4:6);
+    s_filt2 = filter(b_sos, a_sos, s_filt2);
+endfor
+
+% low level second order sections
+s_filt3 = s;
+for i=1:N/2
+    Xk0=0; Xk1=0; Xk2=0; Yk0=0; Yk1=0; Yk2=0;
+    b0 = sos_q_scaled(i,1);
+    b1 = sos_q_scaled(i,2);
+    b2 = sos_q_scaled(i,3);
+    a1 = sos_q_scaled(i,5);
+    a2 = sos_q_scaled(i,6);
+    for k=1:Nsim
+        Xk0 = s_filt3(k);    
+        Yk0 = b0*Xk0 + b1*Xk1 + b2*Xk2 - a1*Yk1 - a2*Yk2; % note - sign on a1, a2.
+        Xk2=Xk1; Xk1=Xk0; Yk2=Yk1; Yk1=Yk0; 
+        s_filt3(k) = Yk0;
+    endfor
+endfor
+
+plot(s_filt, '*r-'); hold on; plot(s_filt3, 'ob-'); hold off;
 
 % print out the coefficients in C++ table. 
 printf("    const int Nsos = %d; // number of second order sections.\n", N/2);
