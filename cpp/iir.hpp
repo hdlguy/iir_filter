562ba2a9bdd0a83e43d5473af119a58d3c124e26
pete <peter.dudley@precisionplanting.com>
2021-08-07 12:15:29 -0400

    created new Cpp version of iir filter to process complex data

--- cpp/iir.hpp
+++ cpp/iir.hpp
@@ -33,10 +33,6 @@ class iir {
     data_type iirfilt(data_type x);
 };
 
-data_type iir::iirfilt(data_type x){
-    return(1.0);
-}
-
 
 iir::iir(){
     // initialize the data delay lines.
@@ -49,3 +45,34 @@ iir::iir(){
 
 }
 
+
+data_type iir::iirfilt(data_type x)
+{
+
+	coeff_type b0, b1, b2, a1, a2;
+
+	data_type temp = x;
+
+	// loop over the Second Order Sections
+	for(int i=0;i<Nsos;i++){
+
+		// get coeffs
+		b0=coeff_array[i][0]; b1=coeff_array[i][1]; b2=coeff_array[i][2]; a1=coeff_array[i][4]; a2=coeff_array[i][5];
+
+		// feed forward shift register
+		x_array[i][2] = x_array[i][1];
+		x_array[i][1] = x_array[i][0];
+		x_array[i][0] = temp;
+
+		// the filter
+		y_array[i][0] = b0*x_array[i][0] + b1*x_array[i][1] + b2*x_array[i][2] - a1*y_array[i][1] - a2*y_array[i][2];
+
+		// feedback shift register
+		y_array[i][2] = y_array[i][1];
+		y_array[i][1] = y_array[i][0];
+		temp = y_array[i][0];
+
+	}
+
+	return(temp);
+}
--- cpp/iir_tb.cpp
+++ cpp/iir_tb.cpp
@@ -7,12 +7,32 @@
 int main()
 {
 
-    iir iir1, iir2;
+    // create the filters
+    iir iir_real, iir_imag;
 
-    data_type y1, y2, x1, x2;
+    // test frequency response with linear fm signal.
+    const int Nsim = 1024*1024;
+    const std::complex<double> j(0.0, 1.0);
 
-    x1 = 1.0; y1 = iir1.iirfilt(x1);
-    x2 = 1.0; y2 = iir2.iirfilt(x2);
+    std::complex<data_type> x, y;
+    double freq, phase, rate;
+    rate = 2.0*M_PI/Nsim; freq = -M_PI; phase = 0.0;
+
+	std::cout << "writing lfm.dat\n";
+	std::ofstream outf2 { "./lfm.dat" };
+	for (int i=0; i<Nsim; i++){
+
+        phase += freq;
+        freq  += rate;
+        x = exp(j*phase);
+
+        y.real(iir_real.iirfilt(x.real())); 
+        y.imag(iir_imag.iirfilt(x.imag()));
+
+        double absval = sqrt( double(y.real())*double(y.real()) + double(y.imag())*double(y.imag()));
+        outf2 << y.real() << "  " << y.imag() << "  " << absval << "\n";
+    }
+
 
 	return(0);
 }
