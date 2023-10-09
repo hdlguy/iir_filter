cdb0bae29c43f910672e32f3affbc8e4c2be7f76
pete <peter.dudley@precisionplanting.com>
2023-10-09 16:29:28 -0400

    12th order high-pass/low-pass filter works in simulation

--- octave/iirgen.m
+++ octave/iirgen.m
@@ -1,6 +1,7 @@
 clear; 
 Nsos = 3; % number of second order sections
-Wc=1/256; 
+Wc_hp = 1/256; 
+Wc_lp = 1/30;
 
 coeff_int = 4;
 coeff_width = 18;
@@ -8,46 +9,31 @@ coeff_frac = coeff_width-coeff_int;
 
 %  iir filter
 
-[b, a] = butter(Nsos*2, Wc, "high"); 
-%[b, a] = besself(Nsos*2,Wc); 
-%Rp = 5; Rs = 20; [b, a] = ellip(Nsos*2, Rp, Rs, Wc, "high");
-%Rp=5; [b, a]=cheby1(Nsos*2, Rp, Wc);
-
-%freqz(b,a,4096);
+%[b, a] = butter(Nsos*2, Wc_hp, "high"); 
+[b, a] = butter(Nsos*2, Wc_lp, "low"); 
 
 % convert filter to second order sections
 [sos, g] = tf2sos(b,a);
-sos(:,1:3) = sos(:,1:3) * (g^(1/Nsos)); % scale the b coefficients by g.
-
-%sec = 4; zplane(sos(sec,1:3), sos(sec,4:6)); 
-
+sos(:,1:3) = sos(:,1:3) * (g^(1/Nsos)); % scale just the b coefficients(1:3) by g.
 
 % quantize coefficients
 sos_q = round(sos*(2^coeff_frac));
 sos_q_scaled = sos_q/(2^coeff_frac);
+
+% check that we have enough integer bits for the coefficients
 max_q_coeff_int = floor(max(max(abs(sos_q_scaled))));
 if (max_q_coeff_int > (2^(coeff_int-2))) printf("ERROR: not enough integer bits.\n"); endif;
 
 % look at the quantized filter coefficients
 [b_q, a_q] = sos2tf(sos_q_scaled);
 %zplane(b_q, a_q);
-%freqz(b_q, a_q, 4096);
 
 % simulate the filter.
 Nsim = 1024;
 s=zeros(1,Nsim); s(5) = 1; % impulse
 
 % direct implementation
-%s_filt = filter(b_q,a_q,s);
 s_filt = filter(b,a,s);
-
-% cascaded SOS
-s_filt2 = s;
-for i=1:Nsos
-    b_sos = sos_q_scaled(i,1:3);
-    a_sos = sos_q_scaled(i,4:6);
-    s_filt2 = filter(b_sos, a_sos, s_filt2);
-endfor
 
 % low level second order sections
 s_filt3 = s;
@@ -68,6 +54,7 @@ endfor
 
 plot(s_filt, '*r-'); hold on; plot(s_filt3, 'ob-'); hold off;
 
+
 % print out the coefficients in C++ table. 
 printf("const int CoeffWordSize = %d;\n", coeff_width);
 printf("const int CoeffIntSize = %d;\n", coeff_int);
@@ -77,7 +64,7 @@ printf("const coeff_type coeff_array[Nsos][6] = {\n");
 for i=1:Nsos
     printf("    { ");
     for j=1:6
-        printf("%+3.10f", sos_q_scaled(i,j));
+        printf("%+3.12f", sos_q_scaled(i,j));
         if (j==6) printf(" "); else printf(", "); endif
     endfor
     if (i==Nsos) printf("}\n"); else printf("},\n"); endif
@@ -94,23 +81,14 @@ printf("    localparam real coeff[0:Nsos-1][0:5] =  {\n");
 for i=1:Nsos
     printf("        '{ ");
     for j=1:6
-        printf("%+3.10f", sos_q_scaled(i,j));
+        printf("%+3.12f", sos_q_scaled(i,j));
         if (j==6) printf(" "); else printf(", "); endif
     endfor
     if (i==Nsos) printf("}\n"); else printf("},\n"); endif
 endfor
 printf("    };\n\n");
 
-%    localparam int  Ndint = 3;
-%    localparam int  Ndfrac = 22;
-%    localparam int  Ncint = 4;
-%    localparam int  Ncfrac = 14;
-%    localparam int  Nsos = 3;
-%    localparam real coeff[0:Nsos-1][0:5] =  {
-%        '{ +0.9921264648, -1.9801635742, +0.9880371094, +1.0000000000, -1.9765014648, +0.9766845703 },
-%        '{ +0.9921264648, -1.9883422852, +0.9962158203, +1.0000000000, -1.9825439453, +0.9826660156 },
-%        '{ +0.9921264648, -1.9842529297, +0.9921264648, +1.0000000000, -1.9935302734, +0.9937133789 }
-%    };
+
 
 
 
--- verilog/iir_filter.sv
+++ verilog/iir_filter.sv
@@ -2,10 +2,14 @@
 module iir_filter #(
     parameter int   Ndint = 3,
     parameter int   Ndfrac = 22,
-    parameter int   Ncint = 4,
-    parameter int   Ncfrac = 14,
-    parameter int   Nsos = 1,
-    parameter real  coeff[0:Nsos-1][0:5] =  {'{97.631072937817507e-03, 195.262145875635014e-03, 97.631072937817507e-03, 1.000000000000000e+00, -942.809041582063173e-03, 333.333333333333258e-03}}
+    parameter int  Ncint  = 4,
+    parameter int  Ncfrac = 14,
+    parameter int  Nsos = 3,
+    parameter real coeff[0:Nsos-1][0:5] =  {
+        '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.8066406250, +0.8165893555 },
+        '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.8521728516, +0.8623657227 },
+        '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.9366455078, +0.9473266602 }
+    }
 )(
     input   logic                   clk,
     input   logic                   dv_in,
