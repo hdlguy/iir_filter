6414b537cb053cc07be95d79011a15f9ad98cb12
pete <peter.dudley@precisionplanting.com>
2023-10-09 17:24:28 -0400

    modified input and output words to 18 bits, 12th order band-pass works well, ready to integrate into an application

--- verilog/iir_filter.sv
+++ verilog/iir_filter.sv
@@ -1,51 +1,46 @@
-//
+// This is an iir filter built from cascaded second order sections.
+// The characteristics of the filter are controlled by parameters.
 module iir_filter #(
-    parameter int   Ndint = 3,
-    parameter int   Ndfrac = 22,
-    parameter int  Ncint  = 4,
-    parameter int  Ncfrac = 14,
-    parameter int  Nsos = 3,
+    parameter int  Ncint  = 4,  // number of coefficient integer bits
+    parameter int  Ncfrac = 14, // number of coefficient fraction bits.
+    parameter int  Nsos = 3,    // number of second order sections
     parameter real coeff[0:Nsos-1][0:5] =  {
         '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.8066406250, +0.8165893555 },
         '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.8521728516, +0.8623657227 },
         '{ +0.0025634766, +0.0051269531, +0.0025634766, +1.0000000000, -1.9366455078, +0.9473266602 }
     }
 )(
-    input   logic                   clk,
-    input   logic                   dv_in,
-    input   logic[Ndint-1:-Ndfrac]  d_in,
-    output  logic                   dv_out,
-    output  logic[Ndint-1:-Ndfrac]  d_out
+    input   logic        clk,
+    input   logic        dv_in,
+    input   logic[17:0]  d_in,
+    output  logic        dv_out,
+    output  logic[17:0]  d_out
 );
 
-    logic   dv[Nsos:0]; // data valid signals between SOS stages.
+    localparam int Ndint = 3;   // number of integer bits in sample data word.
+    localparam int Ndfrac = 22; // number of fraction bits in sample data word.
+    
+    logic dv[Nsos:0]; // data valid signals between SOS stages.
     logic[Ndint-1:-Ndfrac] data[Nsos:0];  // data between SOS stages.
     assign dv[0] = dv_in;
-    assign data[0] = d_in;
+    assign data[0] = {{(Ndint-1){d_in[17]}}, d_in, 5'b00000}; // sign extend and zero pad 18 bit input to 25 bit filter word.
+    
 
     // Second Order Sections
     generate for(genvar i=0; i<Nsos; i++) begin
         iir_sos #(.Ndint(Ndint), .Ndfrac(Ndfrac), .Ncint(Ncint), .Ncfrac(Ncfrac), .coeff(coeff[i])) uut_real (.clk(clk), .dv_in(dv[i]), .d_in(data[i]), .dv_out(dv[i+1]), .d_out(data[i+1]));
     end endgenerate
+    
 
-    assign dv_out = dv[Nsos];
-    assign d_out  = data[Nsos];
+    // round and saturate from 25 bits down to 18 bit output word
+    logic[17:0] dout_sat;
+    round_n_sat #(.Win(Ndint+Ndfrac), .Nround(Ndfrac-17), .Nsat(Ndint-1)) sat_inst (.din(data[Nsos]), .dout(dout_sat));
+    always_ff @(posedge clk) begin
+        dv_out <= dv[Nsos];
+        d_out  <= dout_sat;
+    end
 
 endmodule
 
 /*
-module iir_sos #(
-    parameter int   Ndint = 3,
-    parameter int   Ndfrac = 22,
-    parameter int   Ncint = 4,
-    parameter int   Ncfrac = 14,
-    // for example, [b, a] = butter(2, 0.25, "low"); [b a]
-    parameter real  coeff[0:5] =  {97.631072937817507e-03, 195.262145875635014e-03, 97.631072937817507e-03, 1.000000000000000e+00, -942.809041582063173e-03, 333.333333333333258e-03}
-)(
-    input   logic                   clk,
-    input   logic                   dv_in,
-    input   logic[Ndint-1:-Ndfrac]  d_in,
-    output  logic                   dv_out,
-    output  logic[Ndint-1:-Ndfrac]  d_out
-);
 */
--- verilog/iir_filter_tb.sv
+++ verilog/iir_filter_tb.sv
@@ -2,10 +2,6 @@
 
 module iir_filter_tb ();
 
-    // data word specification
-    localparam int  Ndint = 3;
-    localparam int  Ndfrac = 22;
-
     // coefficient specification
     localparam int  Ncint  = 4;
     localparam int  Ncfrac = 14;
@@ -19,15 +15,14 @@ module iir_filter_tb ();
         '{ +0.002563476562, +0.005126953125, +0.002563476562, +1.000000000000, -1.936645507812, +0.947326660156 }        
     };
 
-    logic                   dv_in;
-    logic[Ndint-1:-Ndfrac]  d_in;
-    logic                   dv_out;
-    logic[Ndint-1:-Ndfrac]  d_out;
+    logic        dv_in;
+    logic[17:0]  d_in;
+    logic        dv_out;
+    logic[17:0]  d_out;
     logic clk=0; localparam time clk_period = 10; always #(clk_period/2) clk=~clk;
 
-    iir_filter #(.Ndint(Ndint), .Ndfrac(Ndfrac), .Ncint(Ncint), .Ncfrac(Ncfrac), .Nsos(Nsos), .coeff(coeff)) uut (.clk(clk), .dv_in(dv_in), .d_in(d_in), .dv_out(dv_out), .d_out(d_out));
+    iir_filter #(.Ncint(Ncint), .Ncfrac(Ncfrac), .Nsos(Nsos), .coeff(coeff)) uut (.clk(clk), .dv_in(dv_in), .d_in(d_in), .dv_out(dv_out), .d_out(d_out));
 
-    localparam int Gpulse = +2**Ndfrac;
     real phase, freq, rate;
     localparam real pi = 3.1419527;
     localparam real chirprate = 2.0*pi/((2.0**22.0)-1.0);
@@ -47,7 +42,7 @@ module iir_filter_tb ();
             dv_in = 1;
             phase = phase + freq;
             freq  = freq  + rate;
-            d_in = (2.0**Ndfrac)*$sin(phase);
+            d_in = ((2.0**17)-1.0)*$sin(phase);
             #(clk_period*1);            
             
         end
@@ -57,18 +52,4 @@ module iir_filter_tb ();
 endmodule
 
 /*
-module iir_filter #(
-    parameter int   Nsos = 1,
-    parameter int   Ndint = 3,
-    parameter int   Ndfrac = 22,
-    parameter int   Ncint = 4,
-    parameter int   Ncfrac = 14,
-    parameter real  coeff[0:Nsos-1][0:5] =  {{97.631072937817507e-03, 195.262145875635014e-03, 97.631072937817507e-03, 1.000000000000000e+00, -942.809041582063173e-03, 333.333333333333258e-03}}
-)(
-    input   logic                   clk,
-    input   logic                   dv_in,
-    input   logic[Ndint-1:-Ndfrac]  d_in,
-    output  logic                   dv_out,
-    output  logic[Ndint-1:-Ndfrac]  d_out,
-);
 */
