b4de7ad3ff354f0c77aa4adc45260f28dc0588be
hdlguy <hdlguy@hdlguy.com>
2024-10-24 17:07:11 -0400

    fixed some tcl scripts for vivado 2024

--- verilog/implement/compile.tcl
+++ verilog/implement/compile.tcl
@@ -3,25 +3,30 @@
 close_project -quiet
 
 open_project proj.xpr
+
 update_compile_order -fileset sources_1
 reset_run synth_1
 launch_runs synth_1 -jobs 8
 wait_on_run synth_1
+
+#source add_ila.tcl
+
 launch_runs impl_1 -jobs 8
-wait_on_run impl_1
-launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_run impl_1
 
 open_run impl_1
-report_timing_summary   -file  ./results/timing.rpt
-report_utilization      -file  ./results/utilization.rpt
+report_timing_summary   -file   ./results/timing.rpt
+report_utilization      -file   ./results/utilization.rpt
+write_debug_probes      -force  ./results/top.ltx
+write_hw_platform -fixed -force -file   ./results/top.xsa
 
-write_debug_probes      -force ./results/top.ltx
-write_bitstream         -force ./results/top.bit
+set_property CFGBVS VCCO [current_design]
+set_property CONFIG_VOLTAGE 2.5 [current_design]
+set_property BITSTREAM.CONFIG.CONFIGRATE 50 [current_design]
+set_property BITSTREAM.Config.SPI_BUSWIDTH 4 [current_design]
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 
-set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [get_designs impl_1]
-write_bitstream         -force ./results/top_x4.bit
-write_cfgmem -force -format mcs -size 16 -interface SPIx4 -loadbit {up 0x00000000 "./results/top_x4.bit" } -file "./results/top.mcs"
+write_bitstream -verbose -force ./results/top.bit
 
 close_project
 
--- verilog/implement/spi_program.tcl
+++ verilog/implement/spi_program.tcl
@@ -1,31 +1,51 @@
+#
+write_cfgmem -force -format MCS -size 128 -interface SPIx4 -loadbit "up 0x0 ./results/top.bit" -verbose ./results/top.mcs
+
 disconnect_hw_server -quiet
 open_hw_manager
 connect_hw_server
-open_hw_target [lindex [get_hw_targets -of_objects [get_hw_servers]] 0]
-current_hw_device [lindex [get_hw_devices] 0]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
-refresh_hw_device [lindex [get_hw_devices] 0]
-#create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_dev  [lindex [get_cfgmem_parts {mt25ql256-spi-x1_x2_x4}] 0]
-create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_dev  [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-refresh_hw_device [lindex [get_hw_devices] 0]
-set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.FILES [list "./results/top.mcs" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0]]
-set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+open_hw_target 
+current_hw_device [get_hw_devices]
+refresh_hw_device [current_hw_device]
 
-startgroup 
-if {![string equal [get_property PROGRAM.HW_CFGMEM_TYPE  [lindex [get_hw_devices] 0]] [get_property MEM_TYPE [get_property CFGMEM_PART [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]]]] }  { create_hw_bitstream -hw_device [lindex [get_hw_devices] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices] 0]]; program_hw_devices [lindex [get_hw_devices] 0]; }; 
-program_hw_cfgmem -hw_cfgmem [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-endgroup
+create_hw_bitstream -hw_device [get_hw_devices] [get_property PROGRAM.HW_CFGMEM_BITFILE [current_hw_device]];
 
-boot_hw_device  [lindex [get_hw_devices] 0]
+create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {s25fl128sxxxxxx0-spi-x1_x2_x4}] 0]
 
+set_property PROGRAM.ADDRESS_RANGE  {use_file}              [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.FILES [list "./results/top.mcs" ]      [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.PRM_FILE {}                            [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none}     [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.BLANK_CHECK    0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.ERASE          1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.CFG_PROGRAM    1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.VERIFY         1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set_property PROGRAM.CHECKSUM       0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+
+program_hw_devices  [current_hw_device]; 
+refresh_hw_device   [current_hw_device];
+
+set status [catch {program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]}]
+
+if {$status != 0} {
+    create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
+    set_property PROGRAM.ADDRESS_RANGE  {use_file}              [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.FILES [list "./results/top.mcs" ]      [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.PRM_FILE {}                            [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none}     [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.BLANK_CHECK    0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.ERASE          1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.CFG_PROGRAM    1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.VERIFY         1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.CHECKSUM       0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+
+    program_hw_devices  [current_hw_device]; 
+    refresh_hw_device   [current_hw_device];
+
+    program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+}
+
+boot_hw_device  [current_hw_device]
 close_hw_manager
+
 
